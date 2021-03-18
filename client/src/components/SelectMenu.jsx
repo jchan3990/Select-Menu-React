cbe04adf89d657cc644ba52c115052a486d71033
jchan3990 <jonchan3990@gmail.com>
2021-03-17 21:32:14 -0700

    Conditional render options

--- client/src/components/SelectMenu.jsx
+++ client/src/components/SelectMenu.jsx
@@ -42,6 +42,30 @@ const SelectMenu = ({ data }) => {
     <svg style={showX} onClick={() => setSearch('')} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" fill="lightgrey"/></svg>
   );
 
+  let optionMenu;
+  if (isVisible && data.filter(option => option[0].toLowerCase().includes(search.toLowerCase())).length > 0) {
+    optionMenu = (
+      <div className="dropdown-menu-container">
+        {data.filter(option => option[0].toLowerCase().includes(search.toLowerCase()))
+          .map((match, i) => (
+            <div className="menu-option" key={i} onClick={() => clickOption(match[0])}>
+              <span style={{marginLeft: "10px"}} key={i}>{match[0]} </span>
+              <img style={{marginRight: "10px"}} src={match[1]} alt={`${match[0]}`} />
+            </div>
+          ))
+        }
+      </div>
+    )
+  } else if (isVisible) {
+    optionMenu = (
+      <div className="dropdown-menu-container">
+        <div className="menu-option">
+          <h1 style={{color: "lightgrey", margin: "0 auto"}}>No Matches</h1>
+        </div>
+      </div>
+    )
+  };
+
   let arrow;
   isVisible ? arrow = upArrow : arrow = downArrow;
 
@@ -63,21 +87,9 @@ const SelectMenu = ({ data }) => {
         <span className="separator"></span>
         {arrow}
       </div>
-      {isVisible &&
-        <div className="dropdown-menu-container">
-          {data.filter(option => option[0].toLowerCase().includes(search.toLowerCase()))
-            .map((match, i) => (
-              <div className="menu-option" key={i} onClick={() => clickOption(match[0])}>
-                <span style={{marginLeft: "10px"}} key={i}>{match[0]} </span>
-                <img style={{marginRight: "10px"}} src={match[1]} alt={`${match[0]}`} />
-              </div>
-            ))}
-        </div>
-      }
+      {optionMenu}
     </div>
   )
 };
 
 export default SelectMenu;
-
-{/* <button style={showX} className="clear-search-btn" onClick={() => setSearch('')}>X</button> */}
