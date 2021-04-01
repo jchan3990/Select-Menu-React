2645ef18d91cbf57c1de27b095eec45292d097ca
jchan3990 <jonchan3990@gmail.com>
2021-04-01 16:40:37 -0700

    Added classname builder

---
+++ single-select/src/utils/classNameBuilder.js
@@ -0,0 +1,36 @@
+/**
+ * Props where only the prop key is used in the className.
+ * @param {*} val A props value
+ * @param {string} key A props key
+ *
+ * @example
+ * <Label tag />
+ * <div class="ui tag label"></div>
+ */
+export const useKeyOnly = (val, key) => val && key;
+
+/**
+ * Props that require both a key and value to create a className.
+ * @param {*} val A props value
+ * @param {string} key A props key
+ *
+ * @example
+ * <Label corner='left' />
+ * <div class="ui left corner label"></div>
+ */
+export const useValueAndKey = (val, key) => val && val !== true && `${val} ${key}`;
+
+/**
+ * Props whose key will be used in className, or value and key.
+ * @param {*} val A props value
+ * @param {string} key A props key
+ *
+ * @example Key Only
+ * <Label pointing />
+ * <div class="ui pointing label"></div>
+ *
+ * @example Key and Value
+ * <Label pointing='left' />
+ * <div class="ui left pointing label"></div>
+ */
+export const useKeyOrValueAndKey = (val, key) => val && (val === true ? key : `${val} ${key}`);
