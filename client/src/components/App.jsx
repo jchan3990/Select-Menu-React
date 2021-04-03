dadbfcd77e0edfe4d541fe0a358cd22a180b3c19
jchan3990 <jonchan3990@gmail.com>
2021-04-03 13:45:44 -0700

    Hide options after pressing enter

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
           name="test"
           placeholder="Car Make"
           value="carMake"
-          data={animeObject}
+          data={options}
           onChange={onChange}
           centered
           striped="lightgrey"
